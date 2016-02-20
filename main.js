804d3764782622929b88e96cd50c093ce5a009ff
ZengYing <vuuvv@qq.com>
2016-02-20 17:06:14 +0800

    init electron project

---
+++ main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+var mainWindow = null;
+
+app.on('window-all-close', function() => {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.webContents.openDevTools();
+  mainWindow.on('close', function() {
+    mainWindow = null;
+  })
+})
