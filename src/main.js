f2c21ffbb6811a3328818bc9bea0d5cae22e6565
ZengYing <vuuvv@qq.com>
2016-02-20 18:46:08 +0800

    add gulp, rollup, babel

---
+++ src/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+
+app.on('window-all-close', () => {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.webContents.openDevTools();
+  mainWindow.on('close', function() {
+    mainWindow = null;
+  })
+})
